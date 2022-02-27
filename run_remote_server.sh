89d956e7f25b9ce580022d32a2f4ae854dd9ea30
bawnorton <bawnorton@me.com>
2022-02-27 12:52:01 -0800

    Initial Commit

--- run_remote_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-sshpass -p "ruth*vapour8NAPKIN" scp ~/Documents/Developer/Python/MovieSync/server.py root@162.248.100.184:/home/python/MovieSyncServer
-sshpass -p "ruth*vapour8NAPKIN" ssh -t root@162.248.100.184 "cd /home/python/MovieSyncServer; ./server.py"
